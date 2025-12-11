e7a1c381fa82f1a89dcdec06e44406af73ddc17a
Marcus <marcusfriberg120@gmail.com>
2025-12-11 17:14:02 +0100

    added input and button

--- js/char_creation.js
+++ js/char_creation.js
@@ -1,1 +1,37 @@
-console.log("hej");
+const characters = [];
+
+function addCharacter() {
+  const nameInput = document.getElementById("newcharacter_name");
+  const name = nameInput.value;
+
+  if (!name) {
+    return;
+  }
+
+  const character = {
+    name: name,
+  };
+
+  characters.push(character);
+
+  nameInput.value = "";
+  listCharacters();
+}
+
+function listCharacters() {
+  const characterListWrapper = document.getElementById("characterlist");
+  characterListWrapper.innerHTML = "";
+
+  characters.forEach((char) => {
+    const characterWrapper = document.createElement("div");
+    const nameElement = document.createElement("h3");
+    nameElement.innerText = char.name;
+
+    characterWrapper.appendChild(nameElement);
+    characterListWrapper.appendChild(characterWrapper);
+  });
+}
+
+document
+  .getElementById("add-new-character")
+  .addEventListener("click", addCharacter);
